c457f6d8d6407ea31a62be5a0bdf7be1448a9361
M3MONs <alan.skrrrz@gmail.com>
2024-02-17 17:14:44 +0100

    add: import PomodoroContent & PomodoroClock components

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,10 +1,15 @@
 import Footer from "components/Footer/Footer";
+import PomodoroClock from "components/PomodoroClock/PomodoroClock";
+import PomodoroContent from "components/PomodoroContent/PomodoroContent";
 
 const { default: Main } = require("components/Main/Main");
 
 function App() {
   return (
     <Main>
+      <PomodoroContent>
+        <PomodoroClock />
+      </PomodoroContent>
       <Footer />
     </Main>
   );
