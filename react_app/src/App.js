525023675bd637a67130fad2f9a501d9122861d3
M3MONs <alan.skrrrz@gmail.com>
2024-02-17 16:35:35 +0100

    add: import Footer component

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,8 +1,11 @@
+import Footer from "components/Footer/Footer";
+
 const { default: Main } = require("components/Main/Main");
 
 function App() {
   return (
     <Main>
+      <Footer />
     </Main>
   );
 }
