82c64f64a869117ab11ff7aeadabcb69cf092d20
M3MONs <alan.skrrrz@gmail.com>
2024-02-18 14:34:05 +0100

    add: handlers for buttons

--- react_app/src/components/PomodoroClock/PomodoroClockBtns.js
+++ react_app/src/components/PomodoroClock/PomodoroClockBtns.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { Button } from 'antd'
 import { ButtonsWrapper } from './PomodoroClock.style'
 
-const PomodoroClockBtns = ({ isStarted }) => {
+const PomodoroClockBtns = ({ isStarted, handleTimerState, handleResetTimer }) => {
     return (
         <ButtonsWrapper>
-            {!isStarted ? <Button>Start</Button> : <Button>Stop</Button>}
-            <Button>Reset</Button>
+            {!isStarted ?
+                <Button onClick={handleTimerState}>Start</Button> :
+                <Button onClick={handleTimerState}>Stop</Button>
+            }
+            <Button onClick={handleResetTimer}>Reset</Button>
         </ButtonsWrapper>
     )
 }
