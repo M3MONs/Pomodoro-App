28d7f78d4f51baf32380470c21622306fa33fdb5
M3MONs <alan.skrrrz@gmail.com>
2024-02-18 14:37:03 +0100

    edit: PomodoroClockWrapper width & color

--- react_app/src/components/PomodoroClock/PomodoroClock.style.js
+++ react_app/src/components/PomodoroClock/PomodoroClock.style.js
@@ -1,11 +1,12 @@
 const { default: styled } = require("styled-components");
 
 const PomodoroClockWrapper = styled.div`
-    width: 300px;
+    width: 450px;
     height: 200px;
     border-radius: 25px;
     padding: 1.5rem;
-    background: rgba(255,255,255,0.6);
+    background: rgba(0,0,0,0.4);
+    color: #d6d6de;
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -15,6 +16,8 @@ const PomodoroClockWrapper = styled.div`
 const ClockWrapper = styled.div`
     font-size: 6rem;
     font-weight: 600;
+    /* -webkit-text-stroke-width: 2px;
+    -webkit-text-stroke-color: black; */
     text-align: center;
 `
 
