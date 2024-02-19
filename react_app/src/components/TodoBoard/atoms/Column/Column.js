a3bdcde2043e7d596f8c0014aa1976d81e6cbbf8
M3MONs <alan.skrrrz@gmail.com>
2024-02-19 21:47:57 +0100

    add: Droppable Task component init

--- react_app/src/components/TodoBoard/atoms/Column/Column.js
+++ react_app/src/components/TodoBoard/atoms/Column/Column.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import ColumnWrapper, { ColumnTasks, ColumnTitle } from './Column.style'
 import Task from '../Task/Task'
+import { Droppable } from 'react-beautiful-dnd'
 
-const Column = ({ children, title = "" }) => {
+const Column = ({ title = "", tasks = [] }) => {
     return (
         <ColumnWrapper>
             <ColumnTitle>{title}</ColumnTitle>
             <ColumnTasks>
-                <Task>Test</Task>
-                {children}
+                {tasks.map((task, index) => (
+                    <Droppable droppableId={title} >
+                        {(provided) => (
+                            <Task
+                                ref={provided.innerRef}
+                                {...provided.droppableProps}
+                                id={task.id}
+                                index={index}
+                            >
+                                {task.content}
+                            </Task>
+                        )}
+                    </Droppable>))}
             </ColumnTasks>
         </ColumnWrapper>
     )
