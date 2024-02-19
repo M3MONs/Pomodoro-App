2896b7c21897ab79ad78b3bb550174d8a2c2c9b5
M3MONs <alan.skrrrz@gmail.com>
2024-02-19 21:49:17 +0100

    edit: move column to useState & pass tasks to Column

--- react_app/src/components/TodoBoard/TodoBoard.js
+++ react_app/src/components/TodoBoard/TodoBoard.js
@@ -1,21 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { DragDropContext } from 'react-beautiful-dnd'
 import { BoardWrapper, ColumnsWrapper } from './TodoBoard.style'
 import Column from './atoms/Column/Column'
 
-const columns = [
-    { id: 1, title: 'To do', tasks: [] },
-    { id: 2, title: 'In progress', tasks: [] },
-    { id: 3, title: 'Done', tasks: [] },
-]
-
 const TodoBoard = () => {
+    // eslint-disable-next-line
+    const [columns, setColumns] = useState([
+        {
+            id: 'column-1',
+            title: 'Do zrobienia',
+            tasks: [
+                {
+                    id: 'task-1',
+                    content: 'Ukończyć zadanie 1',
+                },
+                {
+                    id: 'task-2',
+                    content: 'Rozpocząć zadanie 2',
+                },
+                {
+                    id: 'task-3',
+                    content: 'Ukończyć zadanie 1',
+                },
+                {
+                    id: 'task-4',
+                    content: 'Rozpocząć zadanie 2',
+                },
+            ],
+        },
+        {
+            id: 'column-2',
+            title: 'W toku',
+            tasks: [],
+        },
+        {
+            id: 'column-3',
+            title: 'Gotowe',
+            tasks: [],
+        },
+    ]);
     return (
         <BoardWrapper>
             <DragDropContext>
                 <ColumnsWrapper>
                     {columns.map((column) => (
-                        <Column key={column.id} title={column.title} />
+                        <Column key={column.id} title={column.title} tasks={column.tasks} />
                     ))}
                 </ColumnsWrapper>
             </DragDropContext>
