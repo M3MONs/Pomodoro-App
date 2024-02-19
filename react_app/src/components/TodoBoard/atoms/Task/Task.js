2c2201f92eae2910986a57809cbcf4ebb7750eeb
M3MONs <alan.skrrrz@gmail.com>
2024-02-19 21:48:14 +0100

    add: Draggable Task component

--- react_app/src/components/TodoBoard/atoms/Task/Task.js
+++ react_app/src/components/TodoBoard/atoms/Task/Task.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import { TaskWrapper } from './Task.style'
+import { Draggable } from 'react-beautiful-dnd'
 
-const Task = ({ children }) => {
+const Task = ({ children, id, index }) => {
     return (
-        <TaskWrapper draggable={true}>{children}</TaskWrapper>
+        <Draggable key={id} draggableId={id} index={index}>
+            {(provided) => (
+                <TaskWrapper key={id}
+                    ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}
+                >
+                    {children}
+                </TaskWrapper>
+            )}
+        </Draggable>
     )
 }
 
