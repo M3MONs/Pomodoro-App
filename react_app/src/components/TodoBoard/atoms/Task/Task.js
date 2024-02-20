1e18ce377817578834e8504df0cf3c963399d788
M3MONs <alan.skrrrz@gmail.com>
2024-02-20 17:52:35 +0100

    rm: Draggable & move to Column.js

--- react_app/src/components/TodoBoard/atoms/Task/Task.js
+++ react_app/src/components/TodoBoard/atoms/Task/Task.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import { TaskWrapper } from './Task.style'
-import { Draggable } from 'react-beautiful-dnd'
 
-const Task = ({ children, id, index }) => {
+const Task = ({ children, provided }) => {
     return (
-        <Draggable key={id} draggableId={id} index={index}>
-            {(provided) => (
-                <TaskWrapper key={id}
-                    ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}
-                >
-                    {children}
-                </TaskWrapper>
-            )}
-        </Draggable>
+        <TaskWrapper ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+            {children}
+        </TaskWrapper>
     )
 }
 
