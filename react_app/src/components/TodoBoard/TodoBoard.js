f239fea32d488e113708cb8ba4f6a873697ad6da
M3MONs <alan.skrrrz@gmail.com>
2024-02-23 21:07:02 +0100

    add: display multiple columns from useState(board)

--- react_app/src/components/TodoBoard/TodoBoard.js
+++ react_app/src/components/TodoBoard/TodoBoard.js
@@ -3,43 +3,74 @@ import { DragDropContext } from 'react-beautiful-dnd'
 import { BoardWrapper, ColumnsWrapper } from './TodoBoard.style'
 import Column from './atoms/Column/Column'
 
+const board =
+    [
+        {
+            id: 0, name: 'To Do', tasks: [
+                {
+                    id: 'task-1',
+                    content: 'Task 1',
+                },
+                {
+                    id: 'task-2',
+                    content: 'Task 2',
+                },
+            ]
+        },
+        {
+            id: 1, name: 'In Progress', tasks: [
+                {
+                    id: 'task-3',
+                    content: 'Task 3',
+                },
+                {
+                    id: 'task-4',
+                    content: 'Task 4',
+                },
+            ]
+        },
+        {
+            id: 2, name: 'Done', tasks: [
+                {
+                    id: 'task-5',
+                    content: 'Task 5',
+                },
+                {
+                    id: 'task-6',
+                    content: 'Task 6',
+                },
+            ]
+        },
+    ]
+
 const TodoBoard = () => {
     // eslint-disable-next-line
-    const [columns, setColumns] = useState([
-        {
-            id: 'task-1',
-            content: 'Task 1',
-        },
-        {
-            id: 'task-2',
-            content: 'Task 2',
-        },
-        {
-            id: 'task-3',
-            content: 'Task 3',
-        },
-        {
-            id: 'task-4',
-            content: 'Task 4',
-        },
-
-    ]);
+    const [columns, setColumns] = useState(board);
 
     function handleOnDragEnd(result) {
         if (!result.destination) return;
 
-        const items = Array.from(columns);
+        const items = Array.from(columns[2].tasks);
         const [reorderedItem] = items.splice(result.source.index, 1);
         items.splice(result.destination.index, 0, reorderedItem);
 
-        setColumns(items);
+        const test = columns[2]
+
+        test.tasks = items
+
+        console.log(items)
+
+        setColumns((prevState) => ({
+            ...prevState,
+            [2]: test,
+        }));
     }
 
     return (
         <BoardWrapper>
             <DragDropContext onDragEnd={handleOnDragEnd}>
                 <ColumnsWrapper>
-                    <Column title={"In Progress"} tasks={columns} />
+                    {board.map((x, index) => (<Column title={x.name} tasks={x.tasks} index={index} />))}
                 </ColumnsWrapper>
             </DragDropContext>
         </BoardWrapper>
