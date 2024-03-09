231e5b18815272f607b53cca4e6869668188face
M3MONs <alan.skrrrz@gmail.com>
2024-03-09 23:48:58 +0100

    add: React-Contexify Menu to ColumnWrapper

--- react_app/src/components/molecules/Column/Column.js
+++ react_app/src/components/molecules/Column/Column.js
@@ -1,13 +1,30 @@
 import React from 'react'
 import ColumnWrapper, { ColumnTitle } from './Column.style'
 import ColumnTasks from '../ColumnTasks/ColumnTasks'
+import { Item, Menu, useContextMenu } from 'react-contexify'
+import 'react-contexify/ReactContexify.css';
+
+const MENU_ID = "ADD_TASK"
 
 const Column = ({ title = "", tasks = [], index }) => {
+    const { show } = useContextMenu({ id: MENU_ID })
+
+    const handleContextMenu = (e) => {
+        show({ event: e, props: {} })
+    }
+
     return (
-        <ColumnWrapper key={index}>
-            <ColumnTitle>{title}</ColumnTitle>
-            <ColumnTasks tasks={tasks} title={title} />
-        </ColumnWrapper>
+        <>
+            <ColumnWrapper key={index} onContextMenu={handleContextMenu}>
+                <ColumnTitle>{title}</ColumnTitle>
+                <ColumnTasks tasks={tasks} title={title} />
+            </ColumnWrapper>
+            <Menu id={MENU_ID} maxWidth="5px" animation='scale'>
+                <Item id='add' >
+                    ADD
+                </Item>
+            </Menu>
+        </>
     )
 }
 
