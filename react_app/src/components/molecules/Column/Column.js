d683291f4cb7707942c39cac9fdc557d5780ec9f
M3MONs <alan.skrrrz@gmail.com>
2024-03-10 14:06:15 +0100

    edit: Item -> StyledItem with styled-components

--- react_app/src/components/molecules/Column/Column.js
+++ react_app/src/components/molecules/Column/Column.js
@@ -3,8 +3,15 @@ import ColumnWrapper, { ColumnTitle } from './Column.style'
 import ColumnTasks from '../ColumnTasks/ColumnTasks'
 import { Item, Menu, useContextMenu } from 'react-contexify'
 import 'react-contexify/ReactContexify.css';
+import styled from 'styled-components';
 
 const MENU_ID = "ADD_TASK"
+
+const StyledItem = styled(Item)`
+    font-weight: 700;
+    font-size: .75rem;
+    font-family: "Pacifico";
+`
 
 const Column = ({ title = "", tasks = [], index }) => {
     const { show } = useContextMenu({ id: MENU_ID })
@@ -20,9 +27,9 @@ const Column = ({ title = "", tasks = [], index }) => {
                 <ColumnTasks tasks={tasks} title={title} />
             </ColumnWrapper>
             <Menu id={MENU_ID} maxWidth="5px" animation='scale'>
-                <Item id='add' >
+                <StyledItem id='add' >
                     ADD
-                </Item>
+                </StyledItem>
             </Menu>
         </>
     )
