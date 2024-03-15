50be72883bc50b1a4dc6d1277827123e5626f6b9
M3MONs <alan.skrrrz@gmail.com>
2024-03-15 20:57:35 +0100

    add: clockState for break & focus mode

--- react_app/src/components/organisms/PomodoroClock/PomodoroClock.js
+++ react_app/src/components/organisms/PomodoroClock/PomodoroClock.js
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from 'react'
-import PomodoroClockWrapper, { ClockWrapper } from './PomodoroClock.style'
+import PomodoroClockWrapper, { ClockTitle, ClockWrapper } from './PomodoroClock.style'
 import PomodoroClockBtns from '../../molecules/PomodoroClockBtns'
 import { formatTime } from '../../utils/PomodoroClock.utils'
 
+const FOCUS_TIME = 30 * 60 * 1000
+const BREAK_TIME = 5 * 60 * 1000
+
+
 const PomodoroClock = () => {
-    const [remainingTime, setRemainingTime] = useState(30 * 60 * 1000)
+    const [remainingTime, setRemainingTime] = useState(FOCUS_TIME)
     const [isStarted, setIsStarted] = useState(false)
+    const [clockState, setClockState] = useState('focus')
 
     useEffect(() => {
+        if (remainingTime === 0) {
+            changeClockState()
+            return
+        }
         if (isStarted) {
             const intervalTimer = setInterval(() => {
                 setRemainingTime((prevTime) => prevTime - 1000)
@@ -17,17 +26,37 @@ const PomodoroClock = () => {
         }
     }, [isStarted, remainingTime])
 
+    useEffect(() => {
+        switch (clockState) {
+            case 'focus':
+                // 30 minutes
+                setRemainingTime(FOCUS_TIME)
+                break
+            case 'break':
+                // 5 minutes
+                setRemainingTime(BREAK_TIME)
+                break
+            default:
+                break
+        }
+    }, [clockState])
+
     const handleTimerState = () => {
         setIsStarted(!isStarted)
     }
 
+    const changeClockState = () => {
+        setClockState((prev) => (prev === 'focus' ? 'break' : 'focus'))
+    }
+
     const handleResetTimer = () => {
-        setRemainingTime(30 * 60 * 1000)
+        setRemainingTime(FOCUS_TIME)
         setIsStarted(false)
     }
 
     return (
         <PomodoroClockWrapper>
+            <ClockTitle>{clockState}...</ClockTitle>
             <ClockWrapper>{formatTime(remainingTime)}</ClockWrapper>
             <PomodoroClockBtns isStarted={isStarted} handleTimerState={handleTimerState} handleResetTimer={handleResetTimer} />
         </PomodoroClockWrapper>
