e29d13fe20428d344141b636d6f4469526ae0142
M3MONs <alan.skrrrz@gmail.com>
2024-03-23 12:50:47 +0100

    add: MainWrapper mobile view css & fix width overflow

--- react_app/src/components/pages/Main/Main.style.js
+++ react_app/src/components/pages/Main/Main.style.js
@@ -2,10 +2,19 @@ import styled from 'styled-components'
 import background from 'assets/images/background.png'
 
 const MainWrapper = styled.main`
-width: 100vw;
-height: 100vh;
+max-width: 100vw;
+width: 100%;
+overflow-x: hidden;
+min-height: 100vh;
 background-image: url(${background});
 background-position: right;
+@media (max-width: 1049px){
+    width: 100%;
+    overflow-x: hidden;
+    min-height: 100vh;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
 `
 
 export default MainWrapper
