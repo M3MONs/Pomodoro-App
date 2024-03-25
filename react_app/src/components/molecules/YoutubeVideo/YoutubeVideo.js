8d6e6230c90b8b0f9c7e170c9f38159fcde43b83
M3MONs <alan.skrrrz@gmail.com>
2024-03-25 20:17:51 +0100

    add: save/read videoLink in cookies

--- react_app/src/components/molecules/YoutubeVideo/YoutubeVideo.js
+++ react_app/src/components/molecules/YoutubeVideo/YoutubeVideo.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react'
+import Cookies from 'js-cookie';
 import YoutubeWrapper from './YoutubeVideo.style'
 import YouTube from 'react-youtube'
 import { Input } from 'antd'
 import { extractVideoId, opts } from '../../utils/YoutubeEmbed.utils'
 
 const YoutubeEmbed = () => {
-    const [videoLink, setVideoLink] = useState('mmKguZohAck')
+    const [videoLink, setVideoLink] = useState(Cookies.get('videoLink') || 'mmKguZohAck');
     const [userInputLink, setUserInputLink] = useState('')
 
     const handleEnterLink = (e) => {
         if (e.key === 'Enter') {
-            setVideoLink(extractVideoId(userInputLink))
+            const videoId = extractVideoId(userInputLink)
+            setVideoLink(videoId)
+            Cookies.set('videoLink', videoId, { expires: 7 });
+            setUserInputLink('')
         }
     }
 
