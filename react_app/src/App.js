0f594888fae74613b567d4b213526e95955d8a0e
M3MONs <alan.skrrrz@gmail.com>
2024-05-31 13:10:22 +0200

    refactor: Implement lazy loading for components in App.js

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,26 +1,33 @@
+import { lazy, Suspense } from "react";
+import { Spin } from "antd";
 import BoardNote from "components/molecules/BoardNote/BoardNote";
 import Footer from "components/molecules/Footer/Footer";
 import PomodoroContent from "components/molecules/PomodoroContent/PomodoroContent";
-import YoutubeEmbed from "components/molecules/YoutubeVideo/YoutubeVideo";
-import PomodoroClock from "components/organisms/PomodoroClock/PomodoroClock";
-import TodoBoard from "components/organisms/TodoBoard/TodoBoard";
 import Main from "components/pages/Main/Main";
 import { BoardProvider } from "context/TodoBoardContext";
 
+const LazyPomodoroClock = lazy(() => import("components/organisms/PomodoroClock/PomodoroClock"));
+
+const LazyYoutubeEmbed = lazy(() => import("components/molecules/YoutubeVideo/YoutubeVideo"));
+
+const LazyTodoBoard = lazy(() => import("components/organisms/TodoBoard/TodoBoard"));
+
 function App() {
-  return (
-    <Main>
-      <PomodoroContent>
-        <PomodoroClock />
-        <YoutubeEmbed />
-        <BoardProvider>
-          <TodoBoard />
-        </BoardProvider>
-        <BoardNote />
-      </PomodoroContent>
-      <Footer />
-    </Main>
-  );
+    return (
+        <Main>
+            <PomodoroContent>
+                <Suspense fallback={<Spin />}>
+                    <LazyPomodoroClock />
+                    <LazyYoutubeEmbed />
+                    <BoardProvider>
+                        <LazyTodoBoard />
+                    </BoardProvider>
+                    <BoardNote />
+                </Suspense>
+            </PomodoroContent>
+            <Footer />
+        </Main>
+    );
 }
 
 export default App;
--- react_app/src/components/molecules/YoutubeVideo/YoutubeVideo.js
+++ react_app/src/components/molecules/YoutubeVideo/YoutubeVideo.js
@@ -1,32 +1,33 @@
-import React, { Suspense, useState } from 'react'
-import Cookies from 'js-cookie';
-import YoutubeWrapper from './YoutubeVideo.style'
-import { Input, Skeleton } from 'antd'
-import { extractVideoId, opts } from '../../utils/YoutubeEmbed.utils'
-
-const LazyYoutubeEmbed = React.lazy(() => import('react-youtube'))
+import React, { useState } from "react";
+import Cookies from "js-cookie";
+import YoutubeWrapper from "./YoutubeVideo.style";
+import { Input } from "antd";
+import { extractVideoId, opts } from "../../utils/YoutubeEmbed.utils";
+import YouTube from "react-youtube";
 
 const YoutubeEmbed = () => {
-    const [videoLink, setVideoLink] = useState(Cookies.get('videoLink') || 'mmKguZohAck');
-    const [userInputLink, setUserInputLink] = useState('')
+    const [videoLink, setVideoLink] = useState(Cookies.get("videoLink") || "mmKguZohAck");
+    const [userInputLink, setUserInputLink] = useState("");
 
     const handleEnterLink = (e) => {
-        if (e.key === 'Enter') {
-            const videoId = extractVideoId(userInputLink)
-            Cookies.set('videoLink', videoId, { expires: 7 });
-            setVideoLink(videoId)
-            setUserInputLink('')
+        if (e.key === "Enter") {
+            const videoId = extractVideoId(userInputLink);
+            Cookies.set("videoLink", videoId, { expires: 7 });
+            setVideoLink(videoId);
+            setUserInputLink("");
         }
-    }
+    };
 
     return (
         <YoutubeWrapper>
-            <Suspense fallback={<Skeleton.Image active={true} style={{ width: '450px', height: '220px' }} />}>
-                <LazyYoutubeEmbed opts={opts} videoId={videoLink} />
-            </Suspense>
-            <Input placeholder='Enter YT video link' onChange={(e) => setUserInputLink(e.target.value)} onKeyDown={handleEnterLink} />
+            <YouTube opts={opts} videoId={videoLink} />
+            <Input
+                placeholder='Enter YT video link'
+                onChange={(e) => setUserInputLink(e.target.value)}
+                onKeyDown={handleEnterLink}
+            />
         </YoutubeWrapper>
-    )
-}
+    );
+};
 
-export default YoutubeEmbed
+export default YoutubeEmbed;
