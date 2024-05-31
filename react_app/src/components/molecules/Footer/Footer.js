b5e72c38cf93e46882ef67ad57b209fb84230070
M3MONs <alan.skrrrz@gmail.com>
2024-05-31 14:45:43 +0200

    refactor: Implement lazy loading for Footer component

--- react_app/src/components/molecules/Footer/Footer.js
+++ react_app/src/components/molecules/Footer/Footer.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import FooterWrapper from './Footer.style'
-import GithubLink from '../../atoms/GithubLink'
+import React from "react";
+import FooterWrapper from "./Footer.style";
+import GithubLink from "../../atoms/GithubLink";
 
 const Footer = () => {
-    const currentYear = new Date().getFullYear()
+    const currentYear = new Date().getFullYear();
     return (
         <FooterWrapper>
-            <p>© {currentYear} Copyright: <GithubLink /> | Sound Effect from <a href="https://pixabay.com/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=6346">Pixabay</a></p>
+            <p>
+                © {currentYear} Copyright: <GithubLink /> | Sound Effect from{" "}
+                <a href='https://pixabay.com/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=6346'>
+                    Pixabay
+                </a>
+            </p>
         </FooterWrapper>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
