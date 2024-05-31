f45ccbac32fe04fa681ee2b6ac7620eae8ab6703
M3MONs <alan.skrrrz@gmail.com>
2024-05-31 14:45:14 +0200

    refactor: Update YoutubeEmbed.utils.js for better performance and readability

--- react_app/src/components/utils/YoutubeEmbed.utils.js
+++ react_app/src/components/utils/YoutubeEmbed.utils.js
@@ -1,10 +1,10 @@
 export const opts = {
-    height: '220',
-    width: 'auto',
-}
+    height: "220",
+    width: "auto",
+};
 
 export const extractVideoId = (url) => {
-    const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/
-    const match = url.match(regExp)
-    return match && match[7]
-}
+    const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    const match = url.match(regExp);
+    return match && match[7];
+};
